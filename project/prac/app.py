c27a78b301642e9629436a4537fed5e802f8734c
YoungJae0910 <dbdudwo0910@gmail.com>
2022-10-24 11:08:25 +0900

    marthproject

--- project/prac/app.py
+++ project/prac/app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'This is Home!'
+    return render_template('index.html')
+
+@app.route('/test', methods=['GET'])
+def test_get():
+   title_receive = request.args.get('title_give')
+   print(title_receive)
+   return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
+
+@app.route('/test', methods=['POST'])
+def test_post():
+   title_receive = request.form['title_give']
+   print(title_receive)
+   return jsonify({'result':'success', 'msg': '요청을 잘 받았어용!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=5000,debug=True)
